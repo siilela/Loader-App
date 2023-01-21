131540c8cba55e647626669bb79a14ba69746550
Siiela Mogashoa <sumogashoa@shoprite.co.za>
2023-01-21 16:58:27 +0200

    add code for loader with API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import LoadingSpinner from "./components/spinner";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  // const handleFetch = () => {
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://reqres.in/api/users?page=0")
+      .then((respose) => respose.json())
+      .then((respose) => {
+        setUsers(respose.data);
+        setIsLoading(true);
+        // Optional code to simulate delay
+        setTimeout(() => {
+          setUsers(respose.data);
+          setIsLoading(false);
+        }, 5000);
+      })
+      .catch(() => {
+        setErrorMessage("Unable to fetch user list");
+        setIsLoading(false);
+      });
+  }, []);
+
+  // };
+  const renderUser = (
+    <div className="userlist-container">
+      {users.map((item, index) => (
+        <div className="user-container" key={index}>
+          <img src={item.avatar} alt="" />
+          <div className="userDetail">
+            <div className="first-name">{`${item.first_name}                
+                                   ${item.last_name}`}</div>
+            <div className="last-name">{item.email}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? <LoadingSpinner /> : renderUser}
+      {errorMessage && <div className="error">{errorMessage}</div>}
+      {/* <button onClick={handleFetch} disabled={isLoading}>
+        Fetch Users
+      </button> */}
     </div>
   );
 }
-
-export default App;
